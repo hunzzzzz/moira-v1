a2487aa674b95d4eb82da0b5589ca68d55c5a98b
hunzz <hunzz.study@gmail.com>
2025-01-28 21:15:06 +0900

    프로젝트 초기 파일 업로드

---
+++ src/test/kotlin/com/hunzz/moirav1/MoiraV1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hunzz.moirav1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoiraV1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
