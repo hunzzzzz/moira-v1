5e7738bcc2adcb2c9986c69d2e2592b547cbf4a1
hunzz <hunzz.study@gmail.com>
2025-02-14 09:44:14 +0900

    Relation 엔티티 생성

---
+++ relation-server/src/main/kotlin/com/hunzz/relationserver/domain/relation/model/RelationId.kt
@@ -0,0 +1,10 @@
+package com.hunzz.relationserver.domain.relation.model
+
+import jakarta.persistence.Embeddable
+import java.util.*
+
+@Embeddable
+data class RelationId(
+    val userId: UUID,
+    val targetId: UUID
+)
